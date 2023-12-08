342008b8a14140821236c67c88de0c02a103bfa5
kStor2poche <leo.lagarrigue@orange.fr>
2023-12-08 12:32:21 +0100

    fn open() : done.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::error::Error;
+use std::env;
+
+fn open() -> Result<Vec<u8>, Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    Ok(std::fs::read(args.get(1).expect("Can't open file"))?)
+}
+
 fn main() {
     println!("Hello, world!");
 }
