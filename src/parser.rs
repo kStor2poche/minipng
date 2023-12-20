c8452caf5b87ecf22f97e94536a3dbf9efebe2c7
kStor2poche <leo.lagarrigue@orange.fr>
2023-12-20 19:23:11 +0100

    added getters for Block and Header structs

--- src/parser.rs
+++ src/parser.rs
@@ -40,6 +40,9 @@ impl Header {
 
         Self { width, height, pixel_type }
     }
+    pub fn get_content(&self) -> (u32, u32, u8) {
+        (self.width, self.height, self.pixel_type)
+    }
 }
 
 pub struct Comment {
@@ -59,6 +62,9 @@ impl Comment {
     fn from_raw_data<T>(data: &T, block_length: u32) -> Self where T: Iterator<Item = u8> {
         let content = data.take(block_length as usize).map(|c| c as char).collect::<String>();
         Self { content }
+    }
+    pub fn get_content(&self) -> &String {
+        &self.content
     }
 }
 
